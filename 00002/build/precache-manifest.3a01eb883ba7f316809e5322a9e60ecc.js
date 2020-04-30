d60fa4ef1bc94f7154b55115a261a767d99e3e7c
Will Foster <42978593+WilliamFoster3@users.noreply.github.com>
2020-04-30 16:57:57 -0500

    Add files via upload

---
+++ 00002/build/precache-manifest.3a01eb883ba7f316809e5322a9e60ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba817b61d19676b5e1778275026aec00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d26b318e16bdb86c20",
+    "url": "/static/css/main.94f4a651.chunk.css"
+  },
+  {
+    "revision": "e536da3aebf3fd244beb",
+    "url": "/static/js/2.1e9d5765.chunk.js"
+  },
+  {
+    "revision": "bb8fb1c4ca51bfe3d3f76c8caf6f2c92",
+    "url": "/static/js/2.1e9d5765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d26b318e16bdb86c20",
+    "url": "/static/js/main.6865ca29.chunk.js"
+  },
+  {
+    "revision": "207a4196f1f6bc2c3eb9",
+    "url": "/static/js/runtime-main.059b2c37.js"
+  }
+]);
